7b2dd8ac33641d1e418457de4260e4caa61f545e
Brn Matos <matos.brn@gmail.com>
2022-08-27 14:24:11 -0300

    First commit of Example Download and Getting informations from Html WebSite using Selenium

---
+++ src/main/java/alphanerd/com/br/download/info/html/selenium/example/MainTracker.java
@@ -0,0 +1,128 @@
+package alphanerd.com.br.download.info.html.selenium.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import alphanerd.com.br.download.info.html.selenium.model.ExampleModel;
+
+public class MainTracker {
+
+	public static void main(String[] args)
+			throws InterruptedException, ExecutionException, TimeoutException, IOException {
+    	Path pathFiles = Paths.get("src/files");
+
+		System.setProperty("webdriver.chrome.driver", pathFiles + "/chromedriver.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		int codPageCount = 1;
+		
+		for (int x = 1; x <= 250; x++) {
+			codPageCount = x;
+			
+			String urlStr = "https://data.anbima.com.br/fundos?page="+codPageCount+"&size=100&";
+			String patchTxt = pathFiles+"/file"+codPageCount+".txt";
+
+			driver.navigate().to(new URL(urlStr));
+
+			Thread.sleep(15000);
+			
+			FileWriter myWriter = new FileWriter(patchTxt);
+			String body = "";
+			StringBuilder sb = new StringBuilder();
+
+			try {
+				WebElement lgpdButton = driver.findElement(By.id("zsitc_close"));
+				lgpdButton.click();
+			
+				Thread.sleep(5000);
+
+				WebElement searchButton = driver.findElement(By.id("LGPD_ANBIMA_global_sites__text__btn"));
+				searchButton.click();
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+
+			for (int i = 0; i < 100; i++) {
+				ExampleModel fd = new ExampleModel();
+
+				WebElement weName01 = driver.findElement(By.id("item-title-" + String.valueOf(i)));
+				String strName = weName01.getText().toString().trim().replace(System.lineSeparator(), "");
+				fd.setName(strName);
+				
+				WebElement weCnpj02 = driver.findElement(By.id("cnpj-" + String.valueOf(i)));
+				String strCnpj = weCnpj02.getText().trim().substring(0,15);
+				strCnpj = strCnpj + "-" + checkType(weCnpj02.getText().trim());
+				fd.setCnpj(strCnpj);
+				
+				String strLink = weName01.getAttribute("href");
+				fd.setLink(strLink);
+
+				String strManager03 = "";
+				try {
+					WebElement weManager03 = driver.findElement(By.id("gestor-" + String.valueOf(i)));
+					strManager03 = weManager03.getText().trim().replace("Gestor", "").replace(System.lineSeparator(), "");
+				} catch (Exception e) {
+					strManager03 = "DOIS GESTORES";
+				}
+				fd.setManager(strManager03);
+				
+				WebElement weDetail04 = null;
+				String strDetail04 = "";
+				try {
+					weDetail04 = driver
+							.findElement(By.id("caracteristicaInvestidor-" + String.valueOf(i)));
+					strDetail04 = weDetail04.getText().trim().
+							replace("Característica do investidor", "");
+					
+				} catch (Exception e) {
+					strDetail04 = "DOIS GESTORES".trim().replace(System.lineSeparator(), "");
+				}
+
+				fd.setDescription(strDetail04);
+				
+				sb.append(i);
+				sb.append("&&");
+				sb.append(strName);
+				sb.append("&&"); 
+				sb.append(strCnpj);
+				sb.append("&&"); 
+				sb.append(strLink);
+				sb.append(System.lineSeparator());
+			}
+			
+			body = sb.toString();
+			myWriter.write(body);
+			myWriter.close();
+			
+			System.out.println("----------------------------------------------------------------------");
+			System.out.println("---------------------TXT SUCCESSFULL EXPORTED-------------------------");
+			System.out.println("----------------------------------------------------------------------");
+			
+		}
+		
+		driver.close();
+	}
+	
+	private static String checkType(String type) {
+		if (type.contains("PREVIDÊNCIA")) {
+			return "PREVIDÊNCIA";
+		}else if (type.contains("RENDA FIXA")) {
+			return "RENDA FIXA";
+		}else if (type.contains("AÇÕES")) {
+			return "AÇÕES";
+		}else if (type.contains("MULTIMERCADOS")) {
+			return "MULTIMERCADOS";
+		}else {return "";}
+		
+	}
+
+}
---
+++ src/main/java/alphanerd/com/br/download/info/html/selenium/example/SubTrackerPageToPage.java
@@ -0,0 +1,164 @@
+package alphanerd.com.br.download.info.html.selenium.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import alphanerd.com.br.download.info.html.selenium.model.ExampleModel;
+
+public class SubTrackerPageToPage {
+	
+	public static void main(String[] args)
+			throws InterruptedException, ExecutionException, TimeoutException, IOException {
+		Path pathFiles = Paths.get("src/files");
+
+		System.setProperty("webdriver.chrome.driver", pathFiles + "/chromedriver.exe");
+
+		
+		for(int i =1; i <= 250; i++) {
+			String nameFile = "/file";
+			String extFile = ".txt";
+			String numberPage = String.valueOf(i);
+			String fileRead = pathFiles + nameFile + numberPage + extFile;
+
+			
+			List<String> arrayListStringNames = new ArrayList<String>();
+
+			if (new File(pathFiles+nameFile+numberPage+extFile).exists()) {
+				
+				FileReader fReader = new FileReader(fileRead);
+				BufferedReader bfReader = new BufferedReader(fReader);
+				
+				try {
+				    String line = bfReader.readLine();
+
+				    while (line != null) {
+				    	arrayListStringNames.add(line);
+				        line = bfReader.readLine();
+				    }
+				} finally {
+					bfReader.close();
+				}
+			}
+			
+			List<ExampleModel> arrayListModel = new ArrayList<ExampleModel>();
+			
+			ChromeDriver driver = new ChromeDriver();
+
+			arrayListStringNames.stream().forEach(action -> {
+				
+				ExampleModel exampleTemp = new ExampleModel();
+				
+				String nameExample = "";
+				String registerExample = "";
+				String linkExample = "";
+				
+				String[] vetRead = action.toString().split("&&");
+				
+				nameExample = vetRead[1];
+				registerExample = vetRead[2];
+				linkExample = vetRead[3];
+				
+				try {
+			
+					driver.navigate().to(new URL(linkExample));
+					Thread.sleep(3000);
+					
+					WebElement lgpdButton = driver.findElement(By.id("zsitc_close"));
+					lgpdButton.click();
+				
+					Thread.sleep(1000);
+
+					WebElement searchButton = driver.findElement(By.id("LGPD_ANBIMA_global_sites__text__btn"));
+					searchButton.click();
+					
+					Thread.sleep(1000);
+
+				} catch (Exception e) {
+
+				}
+				String strNameExample = "";
+				String strRegisterExample = "";
+				WebElement weMain = null;
+				try {
+					weMain = driver.findElement(By.id("output__container--prestadoresGestores"));
+					WebElement weNameExample = weMain.findElement(By.className("anbima-ui-output__value"));
+					strNameExample = weNameExample.getText().toString();
+					
+					WebElement weRegisterExample = weMain.findElement(By.className("anbima-ui-output__detail"));
+					strRegisterExample = weRegisterExample.getText().toString();
+
+				} catch (Exception e) {
+					WebElement wePanelButton = driver.findElement(By.id("output__container--prestadoresGestores"));
+
+					WebElement weButtonClick03 = wePanelButton.findElement(By.className("anbima-ui-output__detail--button"));
+					weButtonClick03.click();
+
+					weMain = driver.findElement(By.id("output__container--prestadoresModal-0"));
+					WebElement weNameExample = weMain.findElement(By.className("anbima-ui-output__label"));
+					strNameExample = weNameExample.getText().toString();
+
+					WebElement weRegisterExample = weMain.findElement(By.className("anbima-ui-output__value"));
+					strRegisterExample = weRegisterExample.getText().toString();
+					
+				}
+
+				exampleTemp.setName(nameExample);
+				exampleTemp.setCnpj(registerExample);
+				exampleTemp.setLink(linkExample);
+				exampleTemp.setManager(strNameExample);
+				exampleTemp.setManagerCnpj(strRegisterExample);
+
+				arrayListModel.add(exampleTemp);
+				
+			});
+			
+			driver.close();
+
+			FileWriter myWriter = new FileWriter(pathFiles+nameFile+numberPage+"_new"+extFile);
+			String body = "";
+			StringBuilder sb = new StringBuilder();
+			
+			arrayListModel.stream().forEach(action -> {
+				sb.append(action.getName().toString());
+				sb.append("&&"); 
+				sb.append(action.getCnpj().toString());
+				sb.append("&&"); 
+				sb.append(action.getLink().toString());
+				sb.append("&&"); 
+				sb.append(action.getManager().toString());
+				sb.append("&&"); 
+				sb.append(action.getManagerCnpj().toString());
+				sb.append(System.lineSeparator());
+			});
+			
+			
+			body = sb.toString();
+			myWriter.write(body);
+			myWriter.close();
+			
+			System.out.println("----------------------------------------------------------------------");
+			System.out.println("----------------------------------------------------------------------");
+			System.out.println("----------------------------------------------------------------------");
+			System.out.println("------------------------EXPORTED SUCESSFULL!--------------------------");
+			System.out.println("----------------------------------------------------------------------");
+			System.out.println("----------------------------------------------------------------------");
+			System.out.println("----------------------------------------------------------------------");
+			
+		}
+		
+	}
+	
+}
